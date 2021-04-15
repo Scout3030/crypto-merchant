6fb634ef46c1f75be7e0fef7abb31a30cf637a05
Michael Mathews <michaelmathewssar@gmail.com>
2021-04-15 10:37:09 -0500

    feature | implement new layout

--- resources/views/components/layouts/merchant.blade.php
+++ resources/views/components/layouts/merchant.blade.php
@@ -5,7 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.png') }}">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ asset('fonts/flaticon.css') }}">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <!-- Flaticon -->
+    <link rel="stylesheet" href="{{ asset('fonts/flaticon.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 	<title>CryptoMatix - Dashboard</title>
 </head>
