d5b37bc9e1b12dd4bcc84265a5609ae9a3bb32c0
David Soria <zorobabel.omicron@gmail.com>
2021-04-14 23:40:34 -0500

    Redirect to login after account validation

--- resources/views/auth/account_activated.blade.php
+++ resources/views/auth/account_activated.blade.php
@@ -4,11 +4,27 @@
         <div class="card-body textNoti">
             <div class="media-body">
                 <p class="mb-0">Thanks for verifying the email address<br>
-                You will shortly receive an email with login details to your account.</p>
+                You will be redirected to login screen in <span id="timeLeft">10 seconds</span> automatically OR</p>
                 <br>
                 <br>
                 <p class="mb-0">Click here to <a href="{{ route('auth.showLoginForm') }}">Sign In</a></p>
             </div>
         </div>
     </div>
+    <script>
+      let timeLeft = 10;
+      const countdownTimer = setInterval(() => {
+        if (timeLeft <= 0) {
+          clearInterval(countdownTimer);
+          window.location.replace('{{ route('auth.login') }}');
+        } else {
+          document.getElementById('timeLeft').innerHTML = [
+            timeLeft.toString(),
+            'second'.concat(timeLeft >= 2 ? 's' : '')
+          ].join(' ');
+        }
+
+        timeLeft -=1;
+      }, 1000);
+    </script>
 </x-layouts.guest>
--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -3,9 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-  @if(isset($type) && $type == 'redirect')
-  <meta http-equiv="Refresh" content="10; URL={{ route('auth.login') }}">
-  @endif
 	<link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.png') }}">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
